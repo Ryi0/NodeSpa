cbf9f44c774ea4f0099efb13be66c3d0d23af52d
NotRyi0 <wpaquette455@gmail.com>
2024-04-09 05:53:57 +0000

    asd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // const http= require('http');
 import http from "http";
 
-import {sendHtml} from "./js/FileHandler.js";
+import {sendHtml, sendImage} from "./js/FileHandler.js";
 
 // const {sendHtml} = require("./js/FileHandler");
 // sendHtml(require("./js/FileHandler"));
@@ -14,7 +14,9 @@ import {sendHtml} from "./js/FileHandler.js";
 
 http.createServer(function (req, res) {
     // sendHtml("src/testaxasd.html", res)
-    sendHtml("src/testaxasd.html", res)
+
+    sendImage("assets/img/brewingCoffee.png", res);
+    sendHtml("src/testaxasd.html", res);
     // res.write('Hello World!'); //write a response to the client
     // res.end(); //end the response
 
--- js/FileHandler.js
+++ js/FileHandler.js
@@ -1,5 +1,8 @@
 import fs from "fs";
+import path from "node:path";
+import * as url from "node:url";
 
+// TODO : [MAKE SURE THE PATHS ARE VALID BEFORE QUERY.]
 export function sendHtml(filePath, res) {
     fs.readFile(filePath, 'utf8', (e, htmlContent) => {
         if (e) {
@@ -7,12 +10,23 @@ export function sendHtml(filePath, res) {
             res.end('such a bad, bad, file');
             return;
         }
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        // res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(htmlContent);
-        res.end();
+        // res.end();
     });
 }
-
+export function sendImage(url, res){
+    fs.readFile(url, (e, img)=>{
+        if (e) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('such a bad, bad, file. No img');
+            return;
+        }
+        // res.writeHead(200, {'Content-Type': 'image/png'});
+        res.write(img);
+        res.end();
+    })
+}
 export function sendCss(filePath, res) {
     fs.readFile(filePath, 'utf8', (e, htmlContent) => {
         if (e) {
@@ -20,8 +34,9 @@ export function sendCss(filePath, res) {
             res.end('such a bad, bad, file');
             return;
         }
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, {'Content-Type': 'text/css'});
         res.write(htmlContent);
         res.end();
     });
 }
+
